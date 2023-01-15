ce8213cc0823f7a58686e0239303c56eb9e01f09
Riquelme Maia Rodrigues <riquelmemaia2@gmail.com>
2023-01-15 16:53:12 -0300

    simulating api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,46 @@
+import { APIKey } from "../../config/key";
+import { Container, Movie, MovieList } from "./styles";
+import { useState, useEffect } from "react";
+
 function Home() {
+  const movies = [
+    {
+      id: 1,
+      title: "Spider Man",
+      image_url:
+        "https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg",
+    },
+    {
+      id: 2,
+      title: "Batman",
+      image_url:
+        "https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg",
+    },
+    {
+      id: 3,
+      title: "Avengers",
+      image_url:
+        "https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg",
+    },
+  ];
+
   return (
-    <h1>Hello World!</h1>
-  )
+    <Container>
+      <h1>Movies</h1>
+      <MovieList>
+        {movies.map((movie) => {
+          return (
+            <Movie key={movie.id}>
+              <a href="">
+                <img src={movie.image_url} alt={movie.title}></img>
+              </a>
+              <span>{movie.title}</span>
+            </Movie>
+          );
+        })}
+      </MovieList>
+    </Container>
+  );
 }
 
 export default Home;
